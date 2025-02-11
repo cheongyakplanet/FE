9bf1fe8280056328a8093ae37ac1d912ec465bc9
injulme <injulme0309@naver.com>
2025-02-11 20:54:08 +0900

    feat: home 지도 연동, 컴포넌트 추가
    - card, dialog, form, label, table components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,5 @@
+import { Map } from 'react-kakao-maps-sdk';
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function Home() {
@@ -5,21 +7,26 @@ export default function Home() {
     <div className="grid grid-cols-2 gap-5">
       <Card className="col-span-2">
         <CardHeader>
-          <CardTitle>
-            <h4>가장 인기 있는 지역</h4>
-          </CardTitle>
+          <CardTitle as="h4">가장 인기 있는 지역</CardTitle>
           <CardDescription>로그인 후 원하는 지역을 찜해보세요😉</CardDescription>
         </CardHeader>
-        <CardContent className="grid grid-cols-2">
-          <div>map</div>
+        <CardContent className="grid grid-cols-2 gap-5">
+          <Map // 지도를 표시할 Container
+            id="kakao-map"
+            center={{
+              // 지도의 중심좌표
+              lat: 37.563685889,
+              lng: 126.975584404,
+            }}
+            className="h-[400px] w-full rounded-2xl border shadow-md"
+            level={8} // 지도의 확대 레벨
+          />
           <div>graph</div>
         </CardContent>
       </Card>
       <Card>
         <CardHeader>
-          <CardTitle>
-            <h4>나의 관심지역 리스트</h4>
-          </CardTitle>
+          <CardTitle as="h4">나의 관심지역 리스트</CardTitle>
         </CardHeader>
         <CardContent>
           <div>list</div>
@@ -27,9 +34,7 @@ export default function Home() {
       </Card>
       <Card>
         <CardHeader>
-          <CardTitle>
-            <h4>게시판</h4>
-          </CardTitle>
+          <CardTitle as="h4">게시판</CardTitle>
         </CardHeader>
         <CardContent>
           <div>board</div>
