2f340813f59b83ca1cc711b06513be8b53435dfe
woo427 <annejwkim00@naver.com>
2025-02-16 02:49:31 +0900

    wip: 커뮤니티 UI 작업 중

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { useAllPostStore } from "@/stores/community";
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card";
+import { useEffect } from "react";
+
 export default function community() {
-  return <div>community page</div>;
+  const {contents, allPost} = useAllPostStore();
+
+  useEffect(() => {
+  allPost();
+  }, [contents])
+
+  return (
+    <div>
+    {contents.map((content, index) => (
+      <Card key={index}>
+        <CardHeader>
+          <CardTitle>{content.title}</CardTitle>
+        </CardHeader>
+      </Card>
+    ))}
+    </div>
+  )
+
+
+    // <Card>
+    //   {contents.map((content) => (
+    //     <CardHeader>
+    //     <CardTitle>{content.title}</CardTitle>
+    //   </CardHeader>
+    //   ))}
+    // </Card>;
 }
