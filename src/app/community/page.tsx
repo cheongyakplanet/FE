89fb99aeb96f2da5d159dbd05d7f16d19e04bdc7
woo427 <annejwkim00@naver.com>
2025-02-16 23:35:48 +0900

    wip: 커뮤니티 UI 작업 중

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -2,11 +2,12 @@
 
 import { useAllPostStore } from "@/stores/community";
 
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card";
 import { useEffect } from "react";
 
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table";
+
 export default function community() {
-  const {contents, allPost} = useAllPostStore();
+  const {contents, totalPages, allPost} = useAllPostStore();
 
   useEffect(() => {
   allPost();
@@ -14,22 +15,37 @@ export default function community() {
 
   return (
     <div>
-    {contents.map((content, index) => (
-      <Card key={index}>
-        <CardHeader>
-          <CardTitle>{content.title}</CardTitle>
-        </CardHeader>
-      </Card>
-    ))}
+      <div className="font-bold">커뮤니티</div>
+      <div>
+        <p className="text-right">검색기능</p>
+        <p className="text-left">좋아요 많은 순 / 내가 작성한 게시글 / 등등....</p>
+      </div>
+      
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>번호</TableHead>
+          <TableHead>제목</TableHead>
+          <TableHead>글</TableHead>
+          <TableHead>작성자</TableHead>
+          <TableHead>날짜</TableHead>
+          <TableHead>좋아요 수</TableHead>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        {contents.map((content, index) => (
+          <TableRow key={index}>
+          <TableCell>{content.id}</TableCell>
+          <TableCell>{content.title}</TableCell>
+          <TableCell>{content.content}</TableCell>
+          <TableCell>{content.username}</TableCell>
+          <TableCell>{content.createdAt}</TableCell>
+          <TableCell>좋아요 수</TableCell>
+      </TableRow>
+      ))}
+      </TableBody>
+    </Table>
     </div>
   )
-
-
-    // <Card>
-    //   {contents.map((content) => (
-    //     <CardHeader>
-    //     <CardTitle>{content.title}</CardTitle>
-    //   </CardHeader>
-    //   ))}
-    // </Card>;
 }
--- src/stores/community.ts
+++ src/stores/community.ts
@@ -11,18 +11,20 @@ interface Content {
 
 interface AllPostState {
   contents: Content[],
+  totalPages: number,
   allPost: () => Promise<void>
 
 }
 
 export const useAllPostStore = create<AllPostState>((set, get) => ({
   contents: [],
+  totalPages: 0,
 
   allPost: async () => {
     try {
       const response = await axiosInstance.get("/community/posts");
       if(JSON.stringify(get().contents) !== JSON.stringify(response.data.content))
-        set({contents: response.data.content});
+        set({contents: response.data.content, totalPages: response.data.totalPages});
       console.log("전체 커뮤니티 게시글", response.data);
     } catch(error) {
       console.error("Failed Get All Post: ", error);
