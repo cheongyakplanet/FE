7f70d2ce65290022d8dfe9f1a9a38af7dcf49935
woo427 <annejwkim00@naver.com>
2025-02-24 14:40:45 +0900

    Feat: 커뮤니티 검색기능 추가

--- src/app/community/components/post-table.tsx
+++ src/app/community/components/post-table.tsx
@@ -1,76 +1,95 @@
-"use client";
+'use client';
 
-import { useAllPostStore } from "@/stores/community";
+import { useEffect, useState } from 'react';
 
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table";
-import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
-import { useEffect, useState } from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
-export default function PostTable({sort}: {sort: string}) {
-  const {contents, totalPages, allPost} = useAllPostStore();
-    const [page, setPage] = useState(1);
-  
-    useEffect(() => {
+import { useAllPostStore } from '@/stores/community';
+
+export default function PostTable({ sort, searchWord }: { sort: string; searchWord: string }) {
+  const { contents, totalPages, allPost } = useAllPostStore();
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
     allPost(sort, page - 1);
-    }, [page])
+  }, [page]);
 
-    useEffect(() => {
+  useEffect(() => {
     allPost(sort, page - 1);
-    }, [sort])
+  }, [sort]);
+
+  const filterContents = contents.filter(
+    (content) => content.title.includes(searchWord) || content.content.includes(searchWord),
+  );
 
   return (
     <div>
       <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>번호</TableHead>
-          <TableHead>제목</TableHead>
-          <TableHead>글</TableHead>
-          <TableHead>작성자</TableHead>
-          <TableHead>날짜</TableHead>
-          <TableHead>좋아요 수</TableHead>
-        </TableRow>
-      </TableHeader>
+        <TableHeader>
+          <TableRow>
+            <TableHead>번호</TableHead>
+            <TableHead>제목</TableHead>
+            <TableHead>글</TableHead>
+            <TableHead>작성자</TableHead>
+            <TableHead>날짜</TableHead>
+            <TableHead>좋아요 수</TableHead>
+          </TableRow>
+        </TableHeader>
 
-      <TableBody>
-        {contents.map((content, index) => (
-          <TableRow key={index}>
-          <TableCell>{content.id}</TableCell>
-          <TableCell>{content.title}</TableCell>
-          <TableCell>{content.content}</TableCell>
-          <TableCell>{content.username}</TableCell>
-          <TableCell>{content.createdAt}</TableCell>
-          <TableCell>좋아요 수</TableCell>
-      </TableRow>
-      ))}
-      </TableBody>
-    </Table>
+        <TableBody>
+          {filterContents.map((content, index) => (
+            <TableRow key={index}>
+              <TableCell>{content.id}</TableCell>
+              <TableCell>{content.title}</TableCell>
+              <TableCell>{content.content}</TableCell>
+              <TableCell>{content.username}</TableCell>
+              <TableCell>{content.createdAt}</TableCell>
+              <TableCell>좋아요 수</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
 
-    <Pagination>
-      <PaginationContent>
-
-        <PaginationItem>
-          <PaginationPrevious 
-            onClick={() => { if(page > 1) setPage(page - 1) }}>
-          </PaginationPrevious>
-        </PaginationItem>
-
-        {[...Array(totalPages)].map((_, index) => (
-          <PaginationItem key={index}>
-            <PaginationLink onClick={() => setPage(index+1)} isActive = {index+1 === page}>{index+1}</PaginationLink>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={() => {
+                if (page > 1) setPage(page - 1);
+              }}
+            ></PaginationPrevious>
           </PaginationItem>
-        ))}
 
-        {/* <PaginationItem>
+          {[...Array(totalPages)].map((_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink onClick={() => setPage(index + 1)} isActive={index + 1 === page}>
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          {/* <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem> */}
 
-        <PaginationItem>
-          <PaginationNext onClick={() => { if(page < totalPages) setPage(page + 1)} }></PaginationNext>
-        </PaginationItem>
-
-      </PaginationContent>
-    </Pagination>
+          <PaginationItem>
+            <PaginationNext
+              onClick={() => {
+                if (page < totalPages) setPage(page + 1);
+              }}
+            ></PaginationNext>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
-  )
+  );
 }
