fbcb92d61f6dba1ef0ebdcbd4c94bd1bd3368534
woo427 <annejwkim00@naver.com>
2025-02-26 17:21:44 +0900

    Feat: 커뮤니티 상세페이지, lucide 설치
    - 커뮤니티 상세피이지 UI

---
+++ src/app/community/detail/page.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+
+import { ArrowLeft, Eye, ThumbsDown, ThumbsUp } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Textarea } from '@/components/ui/textarea';
+
+export default function detail() {
+  return (
+    <div className="space-y-10">
+      <Link href="/community" className="flex space-y-1 underline">
+        <ArrowLeft />
+        전체 글 보러가기
+      </Link>
+      <Card>
+        <CardHeader>
+          <div className="flex justify-between">
+            <div>
+              <CardTitle>제목</CardTitle>
+            </div>
+            <div className="flex">
+              <Eye />
+              조회수
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <div>날짜 / 시간</div>
+            <div className="space-x-1">
+              <Button className="bg-red-500">
+                <ThumbsUp />
+                좋아요
+              </Button>
+              <Button className="bg-blue-500">
+                <ThumbsDown />
+                싫어요
+              </Button>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div>내용</div>
+          <div>댓글,대댓글 기능</div>
+        </CardContent>
+        <CardFooter>카드 footer</CardFooter>
+      </Card>
+      <div className="space-y-2">
+        <Textarea placeholder="댓글을 작성해 주세요." />
+        <Button className="w-full">댓글 달기</Button>
+      </div>
+    </div>
+  );
+}
