d105e5fd1dab9383e5e3f51908f3782a9c078a06
Kim jisu <64348312+singingsandhill@users.noreply.github.com>
2025-04-03 18:40:50 +0900

    build: 정적 내보내기 철회
    vercel 사용 시도

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/FE",
-  assetPrefix: "/FE",
+  // output: "export",
+  // basePath: "/FE",
+  // assetPrefix: "/FE",
 };
 
 export default nextConfig;
