b44c78ed08e36f6818b00107dd62b5504d0a96ff
김지우 <annejwkim00@naver.com>
2025-04-15 14:40:53 +0900

    feat: 관심지역 api 연동 #23
    - @tanstack/react-table 활용

--- src/app/mypage/region/page.tsx
+++ src/app/mypage/region/page.tsx
@@ -1,3 +1,50 @@
+'use client';
+
+import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+
+import { useGetMypage } from '@/services/member/hooks/useGetMypage';
+
 export default function Region() {
-  return (<div><div>관심 지역</div></div>);
+  const { data: getMypage } = useGetMypage();
+  const mypage = getMypage?.data;
+  const data = Array.isArray(mypage?.interestLocals)
+    ? mypage.interestLocals.map((local, index) => ({
+        id: index,
+        interestLocal: local,
+      }))
+    : [];
+
+  const table = useReactTable({
+    data,
+    columns: [
+      {
+        accessorKey: 'interestLocal',
+      },
+    ],
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>관심지역</TableHead>
+            <TableHead>삭제</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.original.interestLocal}</TableCell>
+              <TableCell>삭제</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
