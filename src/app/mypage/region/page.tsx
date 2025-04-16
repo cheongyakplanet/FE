ac97151208f44d42077b4c3ca837590a0b822ea0
김지우 <annejwkim00@naver.com>
2025-04-16 21:42:13 +0900

    design: 마이페이지 관심지역 ui 수정 #23

--- src/app/mypage/region/page.tsx
+++ src/app/mypage/region/page.tsx
@@ -6,6 +6,7 @@ import { SquarePlus } from 'lucide-react';
 import { MapPin } from 'lucide-react';
 import { MessageCircleWarning } from 'lucide-react';
 
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 import { useDeleteRegion } from '@/services/member/hooks/useDeleteRegion';
@@ -50,23 +51,42 @@ export default function Region() {
         <div>
           <div>나의 관심 지역 목록</div>
           <div className="text-xs text-gray-400">관심 지역을 기반으로 청약 추천이 제공됩니다.</div>
+
+          {table.getRowModel().rows.length < 5 && (
+            <div>
+              <hr className="mb-2 mt-3 border-t border-gray-200" />
+              <div className="mb-2 flex text-xs text-gray-500">
+                <MessageCircleWarning size={16} />
+                <div>최대 5개까지 등록할 수 있습니다.</div>
+              </div>
+              <div className="mb-3 flex gap-2">
+                <Select>
+                  <SelectTrigger className="h-8 w-40 text-xs">
+                    <SelectValue placeholder="시/도를 선택해 주세요." />
+                  </SelectTrigger>
+                  <SelectContent>{/* <SelectItem></SelectItem> */}</SelectContent>
+                </Select>
+                <Select>
+                  <SelectTrigger className="h-8 w-40 text-xs">
+                    <SelectValue placeholder="군/구를 선택해 주세요." />
+                  </SelectTrigger>
+                  <SelectContent>{/* <SelectItem></SelectItem> */}</SelectContent>
+                </Select>
+                <div className="mr-2 flex w-full items-center justify-end text-gray-600">
+                  <SquarePlus size={22} strokeWidth={1} />
+                </div>
+              </div>
+              <hr className="mt-2 border-t border-gray-200" />
+            </div>
+          )}
+
           <div className="mx-auto w-[500px]">
             <Table>
-              <TableCaption>
-                <div className="flex justify-center text-xs text-gray-400">
-                  <MessageCircleWarning size={16} />
-                  <div>최대 5개까지 등록할 수 있습니다.</div>
-                </div>
-              </TableCaption>
               <TableHeader>
                 <TableRow className="hover:bg-transparent">
                   <TableHead className="w-1/3">시/도</TableHead>
                   <TableHead className="w-1/3">군/구</TableHead>
-                  <TableHead className="flex w-full items-center justify-end">
-                    <button>
-                      <SquarePlus size={22} strokeWidth={1.5} />
-                    </button>
-                  </TableHead>
+                  <TableHead></TableHead>
                 </TableRow>
               </TableHeader>
 
