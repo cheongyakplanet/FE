2bc3c11c8e9541ab5f2d81b8c9a0972d1f70a66c
김지우 <annejwkim00@naver.com>
2025-04-17 14:33:49 +0900

    design: 관심지역 UI 수정 #23

--- src/app/mypage/region/page.tsx
+++ src/app/mypage/region/page.tsx
@@ -7,9 +7,10 @@ import { SquareMinus } from 'lucide-react';
 import { SquarePlus } from 'lucide-react';
 import { MapPin } from 'lucide-react';
 import { MessageCircleWarning } from 'lucide-react';
+import { SquareX } from 'lucide-react';
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 import { useDeleteRegion } from '@/services/member/hooks/useDeleteRegion';
 import { useGetMypage } from '@/services/member/hooks/useGetMypage';
@@ -18,6 +19,8 @@ import { useGetDistrict } from '@/services/region/hooks/useGetDistrict';
 import { useGetRegion } from '@/services/region/hooks/useGetRegion';
 
 export default function Region() {
+  const [showAddForm, setShowAddForm] = useState(false);
+
   const { mutate: postRegion } = usePostInterestRegion();
   const { mutate: deleteRegion } = useDeleteRegion();
   const { data: getRegion } = useGetRegion();
@@ -70,7 +73,7 @@ export default function Region() {
           <div>나의 관심 지역 목록</div>
           <div className="text-xs text-gray-400">관심 지역을 기반으로 청약 추천이 제공됩니다.</div>
 
-          {table.getRowModel().rows.length < 5 && (
+          {/* {table.getRowModel().rows.length < 5 && (
             <div>
               <hr className="mb-2 mt-3 border-t border-gray-200" />
               <div className="mb-2 flex text-xs text-gray-500">
@@ -111,15 +114,31 @@ export default function Region() {
               </div>
               <hr className="mt-2 border-t border-gray-200" />
             </div>
-          )}
+          )} */}
 
           <div className="mx-auto w-[500px]">
             <Table>
+              <TableCaption>
+                <div className="flex justify-center text-xs text-gray-500">
+                  <MessageCircleWarning size={16} />
+                  <div>최대 5개까지 등록할 수 있습니다.</div>
+                </div>
+              </TableCaption>
               <TableHeader>
                 <TableRow className="hover:bg-transparent">
                   <TableHead className="w-1/3">시/도</TableHead>
                   <TableHead className="w-1/3">군/구</TableHead>
-                  <TableHead></TableHead>
+                  {table.getRowModel().rows.length < 5 && (
+                    <TableHead>
+                      <button
+                        onClick={() => setShowAddForm(!showAddForm)}
+                        className="mr-2 flex w-full items-center justify-end text-gray-800"
+                      >
+                        {showAddForm && <SquareX size={22} strokeWidth={1} />}
+                        {!showAddForm && <SquarePlus size={22} strokeWidth={1} />}
+                      </button>
+                    </TableHead>
+                  )}
                 </TableRow>
               </TableHeader>
 
@@ -137,6 +156,50 @@ export default function Region() {
                     </TableCell>
                   </TableRow>
                 ))}
+                {showAddForm && (
+                  <TableRow className="hover:bg-transparent">
+                    <TableCell>
+                      <Select onValueChange={(value) => setSelectedCity(value)}>
+                        <SelectTrigger className="h-8 w-40 text-xs">
+                          <SelectValue placeholder="시/도를 선택해 주세요." />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {getRegion?.map((region: string, index: number) => (
+                            <SelectItem key={index} value={region}>
+                              {region}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <Select onValueChange={(value) => setSelectedDistrict(value)}>
+                        <SelectTrigger className="h-8 w-40 text-xs">
+                          <SelectValue placeholder="군/구를 선택해 주세요." />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {Array.isArray(getDistrict) &&
+                            getDistrict?.map((district: string, index: number) => (
+                              <SelectItem key={index} value={district}>
+                                {district}
+                              </SelectItem>
+                            ))}
+                        </SelectContent>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <button
+                        onClick={() => {
+                          addInterestRegion();
+                          setShowAddForm(!showAddForm);
+                        }}
+                        className="mr-2 flex w-full items-center justify-end text-gray-800"
+                      >
+                        <SquarePlus size={22} strokeWidth={1} />
+                      </button>
+                    </TableCell>
+                  </TableRow>
+                )}
               </TableBody>
             </Table>
           </div>
