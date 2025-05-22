82dca749d96e89a7f21fdc783c9603b6c5c89317
Kim jisu <jisu0259@naver.com>
2025-05-22 14:54:59 +0900

    refactor: Google Analytics 등록 #26

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -7,6 +7,16 @@ import { KAKAO_MAP_API } from '@/components/kakaomap';
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-R9QB2QT9S4" strategy="beforeInteractive" />
+      <Script id="google-analytics" strategy="beforeInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-R9QB2QT9S4');
+        `}
+      </Script>
+
       <Script src={KAKAO_MAP_API} strategy="beforeInteractive" />
       {children}
     </>
